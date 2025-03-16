edb5368297124e8e456c6ab54104c2c049528202
v.krasov.ext <v.krasov@aleria.cloud>
2025-03-16 22:10:35 +0400

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/controller/FileController.java
+++ src/main/java/ru/job4j/dreamjob/controller/FileController.java
@@ -1,10 +1,8 @@
 package ru.job4j.dreamjob.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
